26b7528a40f381de08365a37cbe2f2aa927cbc50
swarupkkk <swarupkkashid.tekhnologia@gmail.com>
2024-05-21 16:06:02 +0530

    fifth commit

--- src/components/afterlogin/AfterLogin.tsx
+++ src/components/afterlogin/AfterLogin.tsx
@@ -1,40 +1,73 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import logo from '../image/bugbear_logo.jpg'
-const AfterLogin=()=>{
-    const [datastore,setdatastore]=useState<any>([]);
+const AfterLogin = () => {
+    const [datastore, setdatastore] = useState<any>([]);
+    const [isHovered, setIsHovered] = useState(false);
+    const [image,setImage]=useState('');
+    const handleChange=(e:ChangeEvent<HTMLInputElement>)=>{
+        console.log(e.target.files)
+    }
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
     useEffect(() => {
         let token = localStorage.getItem('token');
-        
+
         if (token) {
-            setdatastore(token)
+
             // Set the token in the axios default headers
             axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-            
+
         }
     }, []);
 
     const fetchUserProfile = async () => {
         try {
-            const response = await axios.get("http://127.0.0.1:8000/api/user/profile/");
+            const response = await axios.get("http://127.0.0.1:8000/api/user/user-details/");
             // Handle response data here
             console.log("User profile data:", response.data);
+            setdatastore(response.data)
+
         } catch (error) {
             console.error("Error fetching user profile:", error);
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         fetchUserProfile()
-    },[datastore])
-    return(
+    }, [])
+    return (
         <>
-        <div className='h-14  py-1 bg-[#171717] top-0 sticky'>
-                <div>
+            <div className='h-14  py-1 bg-[#171717] top-0 sticky'>
+                <div className="flex">
                     <div className='h-10 w-52 py-1'><img src={logo} alt="" className='h-full w-full' /></div>
+                    <div className="ml-auto"
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        <h1 className="text-xl text-white font-bold mr-14">User</h1>
+                        {isHovered?<div className="relative">
+                            <div className="absolute w-[240px] bg-white text-center right-5">
+                            <h1 className="border-b">Upload your profile photo</h1>
+                            <div className="text-center w-full">
+                                <input type="file" onChange={handleChange} className="text-center mx-2"/>
+                                <button className="border rounded">Upload</button>
+                            </div>
+                            </div>
+                        </div>:<></>}
+                    </div>
                 </div>
             </div>
-    <div className="w-full text-center"><h1 className="text-3xl font-bold text-white">Login Successfully</h1></div>
-    </>
+            <div className="w-full text-center">
+                <h1 className="text-3xl font-bold text-white">Login Successfully</h1>
+                <p className="text-3xl font-bold text-white">Email:{datastore.email}</p>
+                <p className="text-3xl font-bold text-white">user Active:{datastore.is_active}</p>
+                <p className="text-3xl font-bold text-white">user Type:{datastore.user_type_name}</p>
+            </div>
+        </>
     )
 }
 
--- src/components/home/Model.tsx
+++ src/components/home/Model.tsx
@@ -13,17 +13,16 @@ const Model: React.FC<ModelProps> = ({show,setShow}) => {
   return (
     <>
       <div className='fixed inset-0 bg-white bg-opacity-30 z-100 backdrop-blur-sm h-full flex justify-center item-center'>
-        <div className='w-[25%] mt-52 shadow h-[230px] rounded-xl bg-black text-center'>
+        <div className='w-[25%] mt-52 shadow h-[250px] rounded-xl bg-black text-center'>
            <FaRegWindowClose className='ml-auto mr-5 mt-2' color='yellow' onClick={setClose}/> 
           <p className='text-white text-xl mt-5 mb-5'>Enter Your Email</p>
           {/* <p className='text-black mt-5 text-[15px] mb-5'>Enter Varification Code</p> */}
           <div className='w-[80%] m-auto jstify-evenly justify-around mt-5 mb-5 justify-center item-center'>  
-          <input type="text" className='w-full outline-none text-black border border-white rounded' placeholder='Email' />
+          <input type="text" className='w-full outline-none text-black h-11 border border-white rounded' placeholder='Email' />
           </div>
           <div className='w-[80%] m-auto mt-10'>
-          <button className='w-full h-8 rounded-full h-11 text-white bg-yellow-400'><span className='py-1 px-4'>Submit</span></button>
+          <button className='w-full h-11 rounded-full text-white bg-yellow-400 p-2'><span className='py-1'>Submit</span></button>
           </div>
-
         </div>
       </div>
     </>
