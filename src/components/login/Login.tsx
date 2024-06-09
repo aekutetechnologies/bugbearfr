12932555c35cb1b5697844bd8fefd3de0e5169f3
swarupkkk <swarupkkashid.tekhnologia@gmail.com>
2024-06-09 12:38:25 +0530

    eight commit

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -4,6 +4,9 @@ import { FaFacebookF } from "react-icons/fa6";
 import { BsGithub } from "react-icons/bs";
 import Navbar from '../nav/Navbar';
 import { Link, useNavigate } from 'react-router-dom';
+import { Icon } from 'react-icons-kit';
+import { eyeOff } from 'react-icons-kit/feather/eyeOff';
+import { eye } from 'react-icons-kit/feather/eye'
 import logo from '../image/bugbear_logo.jpg'
 import './login.css';
 import { useState } from 'react';
@@ -36,6 +39,10 @@ const Login: React.FC = () => {
     const [creadential, setCreadential] = useState<Credential>({ email: "", password: "" })
     const [SuccessMessage, setSuccessMessage] = useState<string>("")
     const [ErrorMessage, setErrorMessage] = useState<string>("")
+    const [type, setType] = useState<string>('password');
+    const [icon, setIcon] = useState<any>(eyeOff);
+    const [type2, setType2] = useState<string>('password');
+    const [icon2, setIcon2] = useState<any>(eyeOff);
     const [pathstr, setPathstr] = useState<string>("")
     const [loginToken, setLoginToken] = useState<string>("")
     const [res, setRes] = useState<any>([])
@@ -54,7 +61,17 @@ const Login: React.FC = () => {
             setEmail(e.target.value)
         }
     }
-    
+
+    const handleToggle2 = () => {
+        if (type2 === "password") {
+            setIcon2(eye)
+            setType2("text")
+        }
+        else {
+            setType2("password")
+            setIcon2(eyeOff)
+        }
+    }
     const handleSubmit = async () => {
         if (password.length < 1) {
             console.log("toas")
@@ -99,43 +116,76 @@ const Login: React.FC = () => {
 
     return (
         <>
-        
-        <div className='bg-[#171717]'>
-        <div className='h-14  py-1 bg-[#171717] top-0 sticky'>
-                <div>
-                    <div className='h-10 w-52 py-1'><img src={logo} alt="" className='h-full w-full' /></div>
-                </div>
-            </div>
-            <div className='login floating w-[30%] m-auto'>
-                <div className='text-center w-[85%] m-auto'>
-                    <h1 className='font-semibold text-black py-8 text-3xl'>Login</h1>
-                    <input type="email" name='email' placeholder='Username or Email' value={creadential.email} className='w-full border-2 border_input1 border-gray-500 rounded p-2 outline-none md:h-9 mt-3 mb-4' onChange={handelinput} />
-                    <input type="password" name='password' placeholder='Password' value={creadential.password} className='w-full border-2 border_input1 border-gray-500 rounded p-2 outline-none md:h-9 mt-3 mb-4' onChange={handelinput} />
-                    <button className='border border-black hover:bg-black text-black hover:text-white rounded-full w-full  py-2' onClick={handleSubmit}><span className=' px-10'>Submit</span></button>
-                    <div className='w-full flex justify-center mt-5'>
-                        <div className='h-[1px] w-full bg-black mt-3'><span></span></div>
-                        <span className='mx-2'>or</span>
-                        <div className='h-[1px] w-full bg-black mt-3'><span></span></div>
+
+            <div className='bg-[#171717]'>
+            <div className='h-14  py-1 bg-[#171717] top-0 flex sticky justify-center justify-evenly gap-56 md:gap-[650px]'>
+                    <div className=''>
+                        <div className='h-[60px] w-52 py-1'><img src={logo} alt="" className='h-full w-full' /></div>
                     </div>
-                    <div className='flex gap-1.5 justify-center m-auto w-[80%] mt-10 mb-10'>
+                    <div className='text-2xl text-white mr-5 mt-4 '>
+                    <Link to="/login"><span>Login</span></Link>/<Link to='/signupclient'><span>Signup</span></Link>
+                    </div>    
+                </div>
+                <div className='login w-[55%] h-[450px] p-5 m-auto flex justify-center justify-evenly mt-20'>
+                    <div className='text-center mx-3 w-[40%]'>
+                        <div className='h-14 rounded-full border border-black flex justify-center justify-evenly pt-3 mt-16'>
+                            <FcGoogle size={30} />
+                            <span>Sign in with Google</span>
+                        </div>
+                        <div className='h-14 rounded-full border border-black flex justify-center justify-evenly pt-3 mt-4 mb-4'>
+                            <BsGithub size={30} />
+                            <span>Sign in with Github</span>
+                        </div>
+                        <p className='text-black text-[12px] text-center'>
+                            By signing , you agree to the <a href="">Term of Servie</a> and ackonowledge you've read our <a href="">Provacy Policy</a>
+                        </p>
+
+                    </div>
+                    <div className='w-[0.5px] bg-gray-300 h-[300px]'>
+                    </div>
+                    <div className="mx-3 w-[40%] ">
+                        <p className='text-black font-semibold text-3xl text-center'>Log in</p>
+                        <label htmlFor="" className='text-xl text-gray-400'>Email</label>
+                        <input type="email" name='email' placeholder='Username or Email' value={creadential.email} className='w-full border-2 border_input1 border-gray-500 rounded-[10px] p-2 outline-none md:h-11 mt-3 mb-4' onChange={handelinput} />
+                        <div className='flex w-full'>
+                            <label htmlFor="" className='text-xl text-gray-400'>Password</label>
+                            <span className="ml-auto mr-5" onClick={handleToggle2}>
+                                <Icon className="" icon={icon2} size={15} /><span className='pt-3 mx-1'>{type2==="password"?"Hide":"Show"}</span>
+                            </span>
+                        </div>
+                        <input type={type2} name='password' placeholder='Password' value={creadential.password} className='w-full border-2 border_input1 border-gray-500 rounded-[10px] p-2 outline-none md:h-11 mt-3 mb-4' onChange={handelinput} />
+                        <div className='w-full text-right'>
+                            <a href="" className=''>Forget Password</a>
+                        </div>
+                        <div>
+                            <input type="checkbox" className='check' />
+                            <label htmlFor="">Remember me</label>
+                        </div>
+                        <button className='bg-gray-200 text-white font-bold hover:bg-black text-black hover:text-white rounded-full w-full  py-3 mt-5' onClick={handleSubmit}><span className=' px-10'>Log in</span></button>
+                        <button className='text-xl border border-black text-black rounded-full w-full  py-2 mt-5' onClick={handleSubmit}><span className=' px-10'>Create an account</span></button>
+
+                    </div>
+
+                </div>
+                <div className='h-14  py-1 bg-[#171717] top-0 flex justify-center justify-evenly sticky mt-20'>
+                    <div>
+                        <div className='h-[60px] w-52 py-1 ml-5'><img src={logo} alt="" className='h-full w-full' /></div>
+                    </div>
+                    <div className='text-center text-white mx-40'>
+                    <p>Copyrights @ bugbrear</p>
+                    </div>
+    
+                    <div className='flex gap-1.5 justify-center'>
                         <FcGoogle size={30} />
-                        <FaFacebookF size={30} color='blue' />
-                        <BsGithub size={30} />
+                        <FaFacebookF size={30} color='white' />
+                        <BsGithub size={30} color="white"/>
                     </div>
-                    <div className='w-full flex justify-center mt-3 pb-10'>
-                        <div className='h-[2px] w-[18%] bg-black mt-3'><span></span></div>
-                        <span className='mx-2 text-black w-full'>Don't have an Bugbeartfront account? </span>
-                        <div className='h-[2px] w-[18%] bg-black mt-3'><span></span></div>
-                    </div>
-                    <Link to='/selectuser' ><button className='border border-green-400 rounded-full py-2 mb-5'><span className='text-greeen px-10 text-green-400'>Sign Up</span></button></Link>
+                    
                 </div>
+
+                <ToastContainer />
             </div>
-            <div className='h-[130px] bg-white w-[90%] my-10 rounded m-auto text-center'>
-                <p className='text-black py-10'>© 2015 - 2024 Bugbeartfront® Global Inc. • Privacy Policy</p>
-            </div>
-            <ToastContainer />
-        </div>
-            
+
         </>
     )
 }
--- src/components/signuptoclient/SignupClient.tsx
+++ src/components/signuptoclient/SignupClient.tsx
@@ -7,6 +7,10 @@ import logo from '../image/bugbear_logo.jpg'
 import { Link } from 'react-router-dom';
 import { RegisterApi } from '../../api/RegisterApi';
 import { ToastContainer, toast } from 'react-toastify';
+import { PanelRightDashedIcon } from 'lucide-react';
+import { FcGoogle } from 'react-icons/fc';
+import { FaFacebookF } from 'react-icons/fa6';
+import { BsGithub } from 'react-icons/bs';
 const SignupClient = () => {
     const [type, setType] = useState<string>('password');
     const [icon, setIcon] = useState<any>(eyeOff);
@@ -20,19 +24,20 @@ const SignupClient = () => {
     const [Email, setEmail] = useState<string>("")
     const [Password, setPassword] = useState<string>("")
     const [confirmPassword, setConfirmPassword] = useState<string>("")
+    const [rightans, setRightans] = useState<boolean>(true)
     function isValidEmail(email: string) {
         // Define a regular expression pattern for email validation.
         const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        
+
         return pattern.test(email);
     }
     function isValidPassword(password: string) {
         // Define a regular expression pattern for email validation.
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        console.log("result",passwordRegex.test(password))
+        console.log("password result", passwordRegex.test(password))
         return passwordRegex.test(password);
-        
     }
+
     const handlechange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value, checked } = e.target;
         if (name === "tc") {
@@ -68,7 +73,7 @@ const SignupClient = () => {
                 [name]: value
             }));
         }
-        
+
     }
     const handleToggle = () => {
         if (type === "password") {
@@ -91,25 +96,29 @@ const SignupClient = () => {
         }
     }
     const handleSubmit = async () => {
+        let valid = true;
         if (!isValidEmail(Email)) {
             // Email is valid, proceed with form submission.
-            
+
             toast.error("invalid email")
             console.log("email is not correct")
         }
-        else if (!isValidPassword(Password)) {
-            // Email is valid, proceed with form submission.
-            
-            toast.error("invalid password")
+        if (!isValidPassword(Password)) {
+
+            toast.error("Invalid password");
+            setRightans(false);
+            valid = false;
             console.log("password is not correct")
         }
-        else if(confirmPassword!==Password)
-        {
+        if (confirmPassword !== Password) {
             toast.error("Password not match")
-        } 
-        console.log("password:",Password)
-        console.log("password2:",confirmPassword)
-        console.log("email:",Email)
+        }
+        if (!valid) {
+            return;
+        }
+        console.log("password:", Password)
+        console.log("password2:", confirmPassword)
+        console.log("email:", Email)
         try {
             const response = await RegisterApi().REGISTER("register/", creadential);
             setSuccessMessage('Registered successfully!');
@@ -120,25 +129,30 @@ const SignupClient = () => {
                 console.log("Tokennn:", registerToken)
                 localStorage.setItem('token', registerToken)
             }
-            
+
         } catch (error) {
             console.error("Error registering:", error);
             setErrorMessage('Something went wrong while registering. Please try again later.');
         }
+
     }
+    console.log("bb", rightans)
 
     return (
         <>
-        <div className='bg-[#171717]'>
-        <div className='h-14  py-1 bg-[#171717] top-0 sticky'>
-                <div>
-                    <div className='h-10 w-52 py-1'><img src={logo} alt="" className='h-full w-full' /></div>
+            <div className=''>
+                <div className='h-14  py-1 bg-[#171717] top-0 flex sticky justify-center justify-evenly gap-56 md:gap-[650px]'>
+                    <div className=''>
+                        <div className='h-[60px] w-52 py-1'><img src={logo} alt="" className='h-full w-full' /></div>
+                    </div>
+                    <div className='text-2xl text-white mr-5 mt-4 '>
+                    <Link to="/login"><span>Login</span></Link>/<Link to='/signupclient'><span>Signup</span></Link>
+                    </div>    
                 </div>
-            </div>
-            <h1 className='text-3xl font-semibolt text-white text-center'>Sign up to hire talent</h1>
-            <div className='w-[70%] m-auto'>
-                <div className='w-[55%] m-auto'>
-                    {/* <div className='flex justify-center justify-evenly w-full'>
+     
+                <div className='w-[70%] md:w-[25%] bg-white rounded-[15px] m-auto mt-20'>
+                    <div className='w-[90%] m-auto'>
+                        {/* <div className='flex justify-center justify-evenly w-full'>
                         <div className='mr-auto'>
                             <label htmlFor="">First Name</label>
                             <input type="text" placeholder='Username or Email' className='w-full border-2 border_input1 border-black rounded p-2 outline-none md:h-9 mt-3 mb-4' />
@@ -148,34 +162,54 @@ const SignupClient = () => {
                             <input type="text" placeholder='Username or Email' className='w-full border-2 border_input1 border-black rounded p-2 outline-none md:h-9 mt-3 mb-4' />
                         </div>
                     </div> */}
-                    <div className=''>
-                        <label htmlFor="" className='text-white'>Email</label>
-                        <input type="email" name='email' placeholder='Email' className='w-full border-2 border_input1 border-black rounded p-2 outline-none md:h-9 mt-3 mb-4' onChange={handlechange} />
-                    </div>
-                    <div className=''>
-                        <label htmlFor="" className='text-white'>Password</label>
-                        <input type={type} name="password" placeholder="Password" className='w-full border-2 border_input1 border-black rounded p-2 outline-none md:h-9 mt-3 mb-4' onChange={handlechange} /><span className="relative top-4 right-10" onClick={handleToggle}>
-                            <Icon className="absolute mr-10" icon={icon} size={25} />
-                        </span>
-                    </div>
-                    <div className=''>
-                        <label htmlFor="" className='text-white'>Confirm Password</label>
-                        <input type={type2} name="password2" placeholder="Confirm Password" className='w-full border-2 border_input1 border-black rounded p-2 outline-none md:h-9 mt-3 mb-4' onChange={handlechange} /><span className="relative top-4 right-10" onClick={handleToggle2}>
-                            <Icon className="absolute mr-10" icon={icon2} size={25} />
-                        </span>
-                    </div>
-                    <div className='flex mt-5'>
-                        <input type="checkbox" name="tc" className='px-2 largerCheckbox1' onChange={handlechange} />
-                        <p className='text-white px-5'>Yes, I understand and agree to the Upwork Terms of Service , including the User Agreement and Privacy Policy .</p>
+                    <p className='text-2xl font-semibold text-black mb-5 mt-3'>Create an account</p>
+                        <div className=''>
+                            <label htmlFor="" className='text-black'>Email</label>
+                            <input type="email" name='email' placeholder='Email' className='w-full border border_input1 border-black rounded-[14px] p-2 outline-none md:h-11 mt-3 mb-4' onChange={handlechange} />
+                        </div>
+                        <div className=''>
+                            <label htmlFor="" className='text-black'>Password</label>
+                            <input type={type} name="password" placeholder="Password" className='w-full border border_input1 border-black rounded-[14px] p-2 outline-none md:h-11 mt-3 mb-4' onChange={handlechange} /><span className="relative top-4 right-10" onClick={handleToggle}>
+                                <Icon className="absolute mr-10" icon={icon} size={25} />
+                            </span>
+                        </div>
+                        {!rightans && <div><div className='flex gap-2'><li className=''>use 8 or more character</li><li>{"use upper case and lower cse letters(e.g.Aa)"}</li></div>
+                            <div><div className='flex'><li>{"use number(e.g.1234)"}</li><li>{"Use Symbol(e.g.!#@$)"}</li></div></div></div>}
+                        <div className=''>
+                            <label htmlFor="" className='text-black'>Confirm Password</label>
+                            <input type={type2} name="password2" placeholder="Confirm Password" className='w-full border border_input1 border-black rounded-[14px] p-2 outline-none md:h-11 mt-3 mb-4' onChange={handlechange} /><span className="relative top-4 right-10" onClick={handleToggle2}>
+                                <Icon className="absolute mr-10" icon={icon2} size={25} />
+                            </span>
+                        </div>
+                        <div className='flex mt-5'>
+                            <input type="checkbox" name="tc" className='px-2 largerCheckbox1' onChange={handlechange} />
+                            <p className='text-black px-5'>Yes, I understand and agree to the Upwork Terms of Service , including the User Agreement and Privacy Policy .</p>
+                        </div>
+                        <div className='w-full text-center'>
+                            <Link to={pathstr} onClick={handleSubmit}><button className=' px-11 bg-gray-300 hover:bg-[#65a30d] h-11 py-2 rounded-full w-full mb-10 mt-10'>Sign in</button></Link>
+                        </div>
                     </div>
                 </div>
+                
+                <div className='h-14  py-1 bg-[#171717] top-0 flex justify-center justify-evenly sticky mt-10'>
+                    <div>
+                        <div className='h-[60px] w-52 py-1 ml-5'><img src={logo} alt="" className='h-full w-full' /></div>
+                    </div>
+                    <div className='text-center text-white mx-40'>
+                    <p>Copyrights @ bugbrear</p>
+                    </div>
+    
+                    <div className='flex gap-1.5 justify-center'>
+                        <FcGoogle size={30} />
+                        <FaFacebookF size={30} color='blue' />
+                        <BsGithub size={30} />
+                    </div>
+                    
+                </div>
+
+                <ToastContainer />
             </div>
-            <div className='w-full text-center'>
-                <Link to={pathstr} onClick={handleSubmit}><button className=' px-11 bg-[#65a30d] py-2 rounded-full mt-10'>Create my account</button></Link>
-            </div>
-            <ToastContainer/>
-        </div>
-            
+
         </>
     )
 }
